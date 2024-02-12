2b5bd5f16c3912190a03593fdf0150d098e335d3
mjescalante <mjescalante@192.168.13.219>
2024-02-12 10:09:30 +0100

    Inicio de proyecto

---
+++ src/main/java/ies/torredelrey/generador/Generador.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ies.torredelrey.generador;
+
+import ies.torredelrey.modelo.CantidadProducto;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanArrayDataSource;
+import net.sf.jasperreports.view.JasperViewer;
+
+/**
+ *
+ * @author usuario
+ */
+public class Generador {
+    //////////////////////////////  PLANTILLA   //////////////////////////////
+    /*public static void leerInforme(List<> lista, String rutaInforme, String nombreInformeSalida){
+<<<<<<< HEAD
+        try {
+            JasperPrint print;
+            HashMap param = new HashMap();
+            param.put("fecha", LocalDate.now().toString());
+
+            JRDataSource datasource = new JRBeanArrayDataSource(lista.toArray());
+            print = JasperFillManager.fillReport(rutaInforme, param,datasource);
+            JasperExportManager.exportReportToPdfFile(print,nombreInformeSalida);
+            JasperViewer.viewReport(print);
+        } catch (JRException ex) {
+            Logger.getLogger(Generador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }*/
+    
+   /***********GENERAR METODOS PARA LEER INFORMES SEGUN SE NECESITE***********/
+    
+    public static void leerInformeVentasTotales(List<CantidadProducto> lista, String rutaInforme, String nombreInformeSalida){
+
+        try {
+            JasperPrint print;
+            HashMap param = new HashMap();
+            param.put("fecha", LocalDate.now().toString());
+
+            JRDataSource datasource = new JRBeanArrayDataSource(lista.toArray());
+            print = JasperFillManager.fillReport(rutaInforme, param,datasource);
+            JasperExportManager.exportReportToPdfFile(print,nombreInformeSalida);
+            JasperViewer.viewReport(print);
+        } catch (JRException ex) {
+            Logger.getLogger(Generador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+}
+
+        
+   /***********GENERAR METODOS PARA LEER INFORMES SEGUN SE NECESITE***********/
+    
+
